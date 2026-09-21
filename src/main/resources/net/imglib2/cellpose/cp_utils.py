76d23ab12f3a7cec4ee7afe39d8053d281a06522
Jean-Yves TINEVEZ <tinevez@pasteur.fr>
2026-09-21 15:04:18 +0200

    Don't run the label unicity method on 2D images.
    Fix #16

--- src/main/resources/net/imglib2/cellpose/cp_utils.py
+++ src/main/resources/net/imglib2/cellpose/cp_utils.py
@@ -6,18 +6,18 @@
 # %%
 # Redistribution and use in source and binary forms, with or without modification,
 # are permitted provided that the following conditions are met:
-# 
+#
 # 1. Redistributions of source code must retain the above copyright notice, this
 #    list of conditions and the following disclaimer.
-# 
+#
 # 2. Redistributions in binary form must reproduce the above copyright notice,
 #    this list of conditions and the following disclaimer in the documentation
 #    and/or other materials provided with the distribution.
-# 
+#
 # 3. Neither the name of the ImgLib2 nor the names of its contributors
 #    may be used to endorse or promote products derived from this software without
 #    specific prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 # ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 # WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
@@ -35,6 +35,7 @@
 import numpy as np
 import torch
 
+
 def get_torch_device(use_gpu: bool) -> tuple[bool, torch.device]:
     """Check torch device availability and returns a tupple (use_gpu: bool, device: torch.device) using the best available backend: CUDA > MPS > CPU."""
     if not use_gpu:
@@ -42,37 +43,44 @@ def get_torch_device(use_gpu: bool) -> tuple[bool, torch.device]:
 
     if torch.cuda.is_available():
         return True, torch.device("cuda")
-    
+
     if torch.backends.mps.is_available():
         return True, torch.device("mps")
 
     return False, torch.device("cpu")
 
+
 # %%
 def unique_labels(masks):
-    """ Ensure that labels are unique in each slice """
+    """Ensure that labels are unique in each slice"""
     masks = np.asarray(masks)
+
+    # 2D image: do nothing. Labels are already unique in the plane
+    if masks.ndim <= 2:
+        return masks
+
     nz = masks.shape[0]
     max_per_z = masks.reshape(nz, -1).max(axis=1).astype(masks.dtype, copy=False)
     ## Calculate the offsets for each slice
     offsets = np.concatenate(([0], np.cumsum(max_per_z, dtype=masks.dtype)[:-1]))
     offsets = offsets.reshape((nz,) + (1,) * (masks.ndim - 1))
-    ## Offset only positive pixels (labels) 
+    ## Offset only positive pixels (labels)
     masks = np.where(masks > 0, masks + offsets, masks)
     return masks
 
+
 def shuffle_labels(masks):
-    """ Randomize the position of the labels so close value are not necessarily close """
+    """Randomize the position of the labels so close value are not necessarily close"""
     masks = np.asarray(masks)
     labels = np.unique(masks)
     if len(labels) <= 1:
         return masks
-    labels = labels[labels!=0]  ## remove 0
-    
+    labels = labels[labels != 0]  ## remove 0
+
     shuffled_labels = np.random.permutation(labels)
     max_label = labels.max()
     indexes = np.zeros(max_label + 1, dtype=masks.dtype)
     indexes[labels] = shuffled_labels
     rand_masks = indexes[masks]
-    
+
     return rand_masks
