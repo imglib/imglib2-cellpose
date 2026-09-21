1b7e9d763b322f27090c71eb91d646be6f7d1c06
Jean-Yves TINEVEZ <tinevez@pasteur.fr>
2026-09-21 14:53:11 +0200

    Remove duplicate method, which did not support null.

--- src/main/java/net/imglib2/cellpose/CellposeParameters.java
+++ src/main/java/net/imglib2/cellpose/CellposeParameters.java
@@ -287,14 +287,7 @@ public abstract class CellposeParameters
 			this.useGpu = useGpu;
 			return ( B ) this;
 		}
-		
-		@SuppressWarnings( "unchecked" )
-		public B labelUnicity( final boolean labelUnicity )
-		{
-			this.labelUnicity  = labelUnicity;
-			return ( B ) this;
-		}
-		
+
 		@SuppressWarnings( "unchecked" )
 		public B randomizeLabels( final boolean randomizeLabels )
 		{
